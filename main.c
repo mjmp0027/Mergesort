17fd49b9bf03cf7739014b7e594d5a9aced5a36f
manuel <mjmp0027@red.ujaen.es>
2023-02-20 10:58:38 +0100

    Mergesort con inserción

--- main.c
+++ main.c
@@ -3,27 +3,26 @@
 #include "ivector.h"
 
 #define umbral 2
+#define TAM 11
 
 
-void burbuja(int primero, int ultimo, ivector v) {
-    int aux;
-    int para = 1;
-    for (int i = primero; i < ultimo && para; i++) {
-        para = 0;
-        for (int j = primero; j < ultimo; j++) {
-            if (v[j] > v[j + 1]) {
-                aux = v[j];
-                v[j] = v[j + 1];
-                v[j + 1] = aux;
-                para = 1;
-            }
-        }
-    }
-}
 
 void mostrarVectorOrdenado(ivector v, int tam) {
     for (int i = 0; i < tam; ++i) {
         printf("%d ", v[i]);
+    }
+}
+
+void insercion(ivector v, int tam) {
+    int clave, j;
+    for (int i = 1; i < tam; ++i) {
+        clave = *(v + i);
+        j = i - 1;
+        while (j >= 0 && *(v + j) > clave) {
+            *(v + j + 1) = *(v + j);
+            j--;
+        }
+        *(v + j + 1) = clave;
     }
 }
 
@@ -40,16 +39,6 @@ void juntar(int primero, int ultimo, int centro, ivector v) {
         }
         z++;
     }
-//    while (x <= centro) {
-//        aux[z] = v[x];
-//        x++;
-//        z++;
-//    }
-//    while (y <= ultimo) {
-//        aux[z] = v[y];
-//        y++;
-//        z++;
-//    }
     z = 0;
     for (int i = primero; i < ultimo; ++i) {
         v[i] = aux[z];
@@ -63,7 +52,7 @@ void juntar(int primero, int ultimo, int centro, ivector v) {
 void mergeSort(ivector v, int primero, int ultimo) {
     int centro;
     if (primero - ultimo + 1 <= umbral) {
-        burbuja(primero, ultimo, v);
+        insercion(v, TAM);
     } else {
         centro = (primero + ultimo) / 2;
         mergeSort(v, primero, centro);
@@ -75,13 +64,13 @@ void mergeSort(ivector v, int primero, int ultimo) {
 
 int main() {
     printf("Hello, World!\n");
-    ivector v = icreavector(11);
-    for (int i = 0; i < 11; ++i) {
+    ivector v = icreavector(TAM);
+    for (int i = 0; i < TAM; ++i) {
         v[i] = rand() % 12;
     }
-    mergeSort(v, 0, 11);
+    mergeSort(v, 0, TAM);
 
-    mostrarVectorOrdenado(v, 11);
+    mostrarVectorOrdenado(v, TAM);
     free(v);
 
     return 0;
